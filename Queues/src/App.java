960c604fe5c9390de45cc0b5eeefaddd5802d471
Miguel Fung <mjfung1@gmail.com>
2021-12-29 03:50:23 -0500

    queues

---
+++ Queues/src/App.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // -------------------------------------------------
+
+
+        // queues = FIFO data structure. First-In First_Out
+        //          A collection designed for holding elements prior to processing
+        //          Linear data structure
+
+        //          add      =   enqueue, offer()
+        //          remove   =   dequeue, poll()
+
+
+
+        // -------------------------------------------------
+
+        // cannot instantiate queues bc its an interface.
+        // so using class that implements queue.
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("karen");
+        queue.offer("chad");
+        queue.offer("steve");
+        queue.offer("harold");
+
+        System.out.println(queue);
+
+    }
+}
