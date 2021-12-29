ea8d1ef0e32647e08854cfd71d35bf8b8b51058d
Miguel Fung <mjfung1@gmail.com>
2021-12-29 03:57:38 -0500

    added comments

--- Queues/src/App.java
+++ Queues/src/App.java
@@ -15,19 +15,35 @@ public class App {
         //          remove   =   dequeue, poll()
 
 
-
+        // where are queue useful?
+        
+        // 1. Keyboard buffer (letters should appear on the screen in the order they're pressed)
+        // 2. Printer queue (print jobs should be completed in order)
+        // 3. Used in LinkedLists, PriorityQueues, BFS
         // -------------------------------------------------
 
         // cannot instantiate queues bc its an interface.
         // so using class that implements queue.
         Queue<String> queue = new LinkedList<String>();
 
+
+        
         queue.offer("karen");
         queue.offer("chad");
         queue.offer("steve");
         queue.offer("harold");
+        
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
+        System.out.println(queue.contains("harold"));
 
+
+        // System.out.println(queue.peek());
+        
+        queue.poll();
+        queue.poll();
+        queue.poll();
+        queue.poll();
         System.out.println(queue);
-
     }
 }
